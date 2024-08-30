8f7b7e56c2b90c64f72da793519d0a1a76282cbe
Jashaman <jashamanpradhan71@gmail.com>
2024-08-30 09:31:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let div= document.createElement("div");
+// div.innerHTML="hey i am now learning javascript"
+// div.setAttribute ("class","jilu")
+// document.querySelector(".container").before (div)
+//  let button = document.getElementById("btn");
+//  button.addEventListener("dblclick",()=>{
+//     document.getElementById("box").innerHTML=" hey you changed"})
