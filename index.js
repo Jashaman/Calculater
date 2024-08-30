3810962cb3408c4da3acc4f706fbac33b5f62939
Jashaman <jashamanpradhan71@gmail.com>
2024-08-30 09:36:12 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// let div= document.createElement("div");
-// div.innerHTML="hey i am now learning javascript"
-// div.setAttribute ("class","jilu")
-// document.querySelector(".container").before (div)
-//  let button = document.getElementById("btn");
-//  button.addEventListener("dblclick",()=>{
-//     document.getElementById("box").innerHTML=" hey you changed"})
